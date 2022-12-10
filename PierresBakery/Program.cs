20d57a2da0ecd3acc465aa45a9a99311b4964943
Richard <charichard09@gmail.com>
2022-12-09 16:49:40 -0800

    add new WriteLine to clarify prompts

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -11,6 +11,7 @@ namespace PierresBakery
       Console.WriteLine("\n-------------------------------------------------------------------------------------------------------------");
       Console.WriteLine("| Welcome to Pierre's Bakery");
       Console.WriteLine("| Here are Today's prices");
+      Console.WriteLine("-------------------------------------------------------------------------------------------------------------");
       Console.WriteLine("| Bread: A single loaf costs $5                        | Pastry: A single pastry for $2");
       Console.WriteLine("| Buy 2, get 1 free (every 3rd loaf of bread is free.) | Buy 3 get $1 off (every 3rd pastry we take $1 off)");
       Console.WriteLine("-------------------------------------------------------------------------------------------------------------");
